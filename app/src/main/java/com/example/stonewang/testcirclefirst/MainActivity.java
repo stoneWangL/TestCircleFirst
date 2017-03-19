c0f466be4c7b475312341fd2e555aa0db41aaa20
stoneWangL <stoneWang_L@163.com>
2017-03-19 22:18:46 +0800

    完成

--- app/src/main/java/com/example/stonewang/testcirclefirst/MainActivity.java
+++ app/src/main/java/com/example/stonewang/testcirclefirst/MainActivity.java
@@ -2,12 +2,21 @@ package com.example.stonewang.testcirclefirst;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
-
+    private CirclePercentView mCirclePercentView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mCirclePercentView = (CirclePercentView) findViewById(R.id.circlePercentView);
+        mCirclePercentView.setOnCircleClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                float percent = (float) (Math.random() * 99 + 1);
+                mCirclePercentView.setCurPercent(percent);
+            }
+        });
     }
 }
